70033240b85e6332f79134e102a85083954c00ab
frederic <olivefdev@gmail.com>
2018-10-20 22:19:56 +0200

    first push

---
+++ build/precache-manifest.00b0c59c7613d4ef659efc05d23625cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51590eba9e1118e8268a",
+    "url": "/static/js/main.51590eba.chunk.js"
+  },
+  {
+    "revision": "795796cf6b7c8773de21",
+    "url": "/static/js/1.795796cf.chunk.js"
+  },
+  {
+    "revision": "51590eba9e1118e8268a",
+    "url": "/static/css/main.06676326.chunk.css"
+  },
+  {
+    "revision": "2bdd3372fa9a4492bf9d03bb24579f19",
+    "url": "/index.html"
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import "./app.css"
+
+const apiKey = process.env.REACT_APP_API_KEY || "AIzaSyA0d-9Xa0sIbn75OdH8EfKg2m_RxSVJORg"
+const projectId = process.env.REACT_APP_PROJECT_ID || "todos-c769e"
+
+var config = {
+    apiKey: "AIzaSyBgjQ-aMPoMk4y802oM57HUwAFSEBBlk10",
+    authDomain: "yo-todo-d1229.firebaseapp.com",
+    databaseURL: "https://yo-todo-d1229.firebaseio.com",
+    projectId: "yo-todo-d1229",
+    storageBucket: "yo-todo-d1229.appspot.com",
+    messagingSenderId: "471773751736"
+  };
+firebase.initializeApp(config)
+
+const db = firebase.firestore()
+db.settings({ timestampsInSnapshots: true })
+
+class App extends Component {
+
+  state = {
+    inputValue: '',
+    todos: []
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    const todo = {
+      value: this.state.inputValue,
+      quantity: 1,
+      archived: false
+    }
+
+    db.collection('todos').doc(todo.value).set(todo)
+    e.preventDefault()
+    this.setState({ inputValue: ''})
+  }
+
+  handleChange = e => {
+    this.setState({ inputValue: e.target.value })
+  }
+
+
+  componentDidMount() {
+    // update state when 'todos' collection changes
+    this.unsubscribe = db.collection('todos')
+      .onSnapshot(snapshot => {
+        const todos = []
+        snapshot.forEach(doc => todos.push(doc.data()))
+
+        this.setState({ todos })
+      })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="shop">
+          <div  className="block">
+            <h2>Shopping list</h2>
+             <form onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
+              <input type="submit" value="add" />
+            </form>
+          </div>  
+          <div className="block">
+            { this.state.todos.map((todo, i) =>
+              <ul key={i}>
+                <li>
+                  <button onClick={e => db.collection('todos').doc(todo.value).update({ quantity: todo.quantity - 1 })}>-</button>
+                  <span>( {todo.quantity} )</span>
+                  <button onClick={e => db.collection('todos').doc(todo.value).update({ quantity: todo.quantity + 1 })}>+</button>
+                  <span className={ todo.archived ? "archived" : "" } onClick={e => db.collection('todos').doc(todo.value).update({ archived : !todo.archived })}> {todo.value} </span>
+                  <button onClick={e => db.collection('todos').doc(todo.value).delete()}>x</button>
+                </li>
+              </ul>) }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default App
